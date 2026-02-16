8613c34765417235109b4cd50e1ec5f78ba05627
Drishti Bansal <drishtibansal06@gmail.com>
2026-02-16 12:25:59 +0530

    Added some fields in personalised section

--- src/FrontEnd/frontend.py
+++ src/FrontEnd/frontend.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from learn_auth_engine import recommend_videos
-import backend 
+# from learn_auth_engine import recommend_videos
+# import backend 
 
 st.set_page_config(page_title="LearnAuth",page_icon="Ace.png",layout="wide")
 left_col, right_col = st.columns([2, 1])
@@ -19,8 +19,23 @@ def popup():
             st.markdown("**Choose your mode of learning:**")
         with col4:
             st.session_state.mode = st.selectbox("", ["Practical", "Theoretical with notes", "A mix of both"], key = "mode_select")
+        col5, col6 = st.columns([3, 4])
+        with col5:
+            st.markdown("**Your current knowledge level:**")
+        with col6:
+            st.session_state.knowledge_level = st.selectbox("", ["Beginner", "Intermediate", "Advanced"], key = "knowledge")    
+        col7, col8 = st.columns([2, 4])
+        with col7:
+            st.markdown("**Learning Goal:**")
+        with col8:
+            st.session_state.learning = st.selectbox("", ["Exam Preparation", "Concept Understanding", "Project Building", "Interview Preparation"], key = "learning_goal")   
+        col9, col10 = st.columns([2, 6])
+        with col9:
+            st.markdown("**Ideal video length:**")
+        with col10:
+            st.session_state.video = st.selectbox("", ["15-20 minutes", "Low tolerance for slow learners", "Often watches at 1.5x speed"], key = "video_length")
         if st.button("Apply", type = "primary"):
-            st.session_state.selected_filters = [st.session_state.language, st.session_state.mode]
+            st.session_state.selected_filters = [st.session_state.language, st.session_state.mode, st.session_state.knowledge_level, st.session_state.learning, st.session_state.video]   
 
 # LEFT SIDE 
 with left_col:
@@ -59,11 +74,11 @@ with right_col:
         placeholder="Machine Learning, Python, Data Structures...",
         key="right_search")
     analyse_button_right = st.button("Analyse Video", key="right_button")
-    if analyse_button_right and topic:
-        with st.spinner("Running backend analysis..."):
-            ranked_df = recommend_videos(topic)
-            if ranked_df is None:
-                st.error("No suitable videos found.")
-            else:
-                st.success("Backend Output Generated")
-                st.dataframe(ranked_df, use_container_width=True)
+    # if analyse_button_right and topic:
+    #     with st.spinner("Running backend analysis..."):
+    #         ranked_df = recommend_videos(topic)
+    #         if ranked_df is None:
+    #             st.error("No suitable videos found.")
+    #         else:
+    #             st.success("Backend Output Generated")
+    #             st.dataframe(ranked_df, use_container_width=True)
